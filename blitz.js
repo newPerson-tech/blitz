17d48f57835cddf3e7c43ab9ac395429f2d150b7
Tetiana Paniot <tatyana.paniot@gmail.com>
2023-12-15 21:39:05 +0000

    Update blitz.js

--- blitz.js
+++ blitz.js
@@ -1,10 +1,10 @@
-let circleRadius = +prompt('Give us any radius length of a circle', ' ');
+let circleRadius = +prompt('Give us any radius length of a circle', 0);
 
 function calculateCircleArea(radius){
     const areaResult = Math.PI * Math.pow(radius, 2);
-    return areaResult;
+    return areaResult.toFixed(2);
 }
 
 const area = calculateCircleArea(circleRadius);
 
-alert(`The area of a circle you've requested is Math.round(${area})`);
+alert(`The area of a circle you've requested is ${area}`);
